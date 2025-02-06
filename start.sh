f8ca3de63b0eba5048fd9247e710eb013c24a1e4
Hollósi Gergely <hollosigergely@gmail.com>
2025-02-06 12:53:45 +0100

    add 12.8 cuda docker file

--- start.sh
+++ start.sh
@@ -2,6 +2,5 @@
 
 echo "Doing docker trick..."
 export FASTRTPS_DEFAULT_PROFILES_FILE=$(pwd)/docker_trick.xml
-#. /opt/ros/humble/setup.bash
 . install/setup.bash
 ros2 run pose_estimation solver /asset/config.yaml
